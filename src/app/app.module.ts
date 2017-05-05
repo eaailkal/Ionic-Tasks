d4f4ff239948c7ad808f3f8a57e02ad4ae7515d1
eaailkal <eaailkal@students.eaaa.dk>
2017-05-05 16:28:12 +0530

    connect to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDW4r711eDdzQYNUcXuX2I-uVRGXFyOjd0",
+    authDomain: "tasks-8fd38.firebaseapp.com",
+    databaseURL: "https://tasks-8fd38.firebaseio.com",
+    projectId: "tasks-8fd38",
+    storageBucket: "tasks-8fd38.appspot.com",
+    messagingSenderId: "810885243354"
+};
+
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
@@ -5,6 +14,9 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+// import the actual components from the angularfire2
+import { AngularFireModule } from 'angularfire2';
 
 // import { HomePage } from '../pages/home/home';
 import { TaskListPage } from '../pages/tasklist/tasklist';
@@ -18,7 +30,8 @@ import { TaskListPage } from '../pages/tasklist/tasklist';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
