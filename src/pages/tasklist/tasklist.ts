15e846523bb6908ca1d9aefb912773d52e06a49d
eaailkal <eaailkal@students.eaaa.dk>
2017-05-05 16:39:40 +0530

    using firebase data

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -12,6 +12,9 @@ import { Component } from '@angular/core';
 // import { NavController } from 'ionic-angular';
 import {NavController, ItemSliding} from 'ionic-angular';
 
+// import AngularFire and FirebaseListObservable modules from the AngularFire 2 library
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
 // we need to inject this class definition
 import { Task } from './task';
 
@@ -24,15 +27,13 @@ import { Task } from './task';
 })
 export class TaskListPage {
 
-  tasks: Array<Task> = [];
+  tasks: FirebaseListObservable<any[]>;
 
-  constructor(public navCtrl: NavController)  {
-  this.tasks = [
-          {title:'Milk', status: 'open'},
-          {title:'Eggs', status: 'open'},
-          {title:'Syrup', status: 'open'},
-          {title:'Pancake Mix', status: 'open'}
-          ];
+  // Our component is going to be using the AngularFire library to communicate with our Firebase database. In order to do this, we will need to pass a reference to that library as a parameter in our constructor
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+  
+  // Since our list of tasks is going to be stored remotely, we can replace our initial tasks array with the AngularFire request to return the data in the tasks sub-directory
+  this.tasks = af.database.list('/tasks');
   }
 
   addItem() {
